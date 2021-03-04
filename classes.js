e0210285a19aebeb2949a52b6b6c54dd395e4791
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-03-04 22:05:05 +0200

    class URL

--- classes.js
+++ classes.js
@@ -4,3 +4,13 @@ class DataBase {
 		this.userIDs = userIds;
 	}
 }
+
+class Url {
+	constructor(long, short, clickCount) {
+		this.long = long;
+		this.short = short;
+		this.clickCount = clickCount;
+	}
+}
+
+module.exports = { DataBase, Url };

bec5f6eb7a8f53df526a1b6cbd1cee87ea1a2e1b
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-03-05 13:25:01 +0200

    structure PUT to /api/shorturl/

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
 
-const shortener = require("./shortener");
+const shorturlRoutes = require("./routes/shorturlRoutes");
 
 app.use(cors());
 app.use(express.json());
 app.use("/public", express.static(`./public`));
 
-app.use(shortener.router);
+app.use("/api/shorturl", shorturlRoutes.router);
 
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/views/index.html");
--- routes/shorturlRoutes.js
+++ routes/shorturlRoutes.js
@@ -6,6 +6,11 @@ let router = express.Router();
 
 router.use(cors());
 
-// PUT /api/shorturl/
+// PUT /api/shorturl
+router.put("/", async (req, res) => {
+	// error handling
+	// generate some kind of id for the new shortURL
+	// return the URL object with short and long url
+});
 
 module.exports = { router };

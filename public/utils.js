0295634929356a3ea6da4e4edc012f62b0dcf811
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-03-06 14:38:26 +0200

    Create utils.js
    function to fetch the data from /api/shorturl with axios to the client.

---
+++ public/utils.js
@@ -0,0 +1,15 @@
+const URL = "http://localhost:3000";
+
+async function makeUrlShort(longUrl) {
+	try {
+		const { data } = await axios({
+			method: "PUT",
+			url: `${URL}/api/shorturl`,
+			data: JSON.stringify({ longUrl }),
+			headers: { "Content-Type": "application/json" },
+		});
+		return data.short;
+	} catch (error) {
+		console.log(error.message);
+	}
+}

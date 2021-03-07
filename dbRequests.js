1eb93b8ef676dc089b25f865ce600ac6967d2edc
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-03-07 05:42:51 +0200

    add env variables to use different db for tests

--- dbRequests.js
+++ dbRequests.js
@@ -1,7 +1,10 @@
 const axios = require("axios");
+require("dotenv").config({ path: __dirname + "/sample.env" });
 
-// This is Koren's default bin which is better for persistency because it always exists
-const MY_BIN_ID_FOR_URLS = "default";
+// This is Koren's default bin which is better for persistency because it always exists. If testing should use the test bin which is also one built in to his package.
+const MY_BIN_ID_FOR_URLS = process.env.DB_URI || "default";
+console.log(process.env.DB_URI);
+console.log(MY_BIN_ID_FOR_URLS);
 
 // using Koren's package of jsonbin-clone to get my relevant bin of urls
 const getUrlsBin = async () => {

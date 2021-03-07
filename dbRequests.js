c2352324e67a14c00159a77073027ffd89542805
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-03-07 06:08:15 +0200

    fix

--- dbRequests.js
+++ dbRequests.js
@@ -3,15 +3,15 @@ require("dotenv").config({ path: __dirname + "/sample.env" });
 
 // This is Koren's default bin which is better for persistency because it always exists. If testing should use the test bin which is also one built in to his package.
 const MY_BIN_ID_FOR_URLS = process.env.DB_URI || "default";
-console.log(process.env.DB_URI);
 console.log(MY_BIN_ID_FOR_URLS);
+console.log();
 
 // using Koren's package of jsonbin-clone to get my relevant bin of urls
 const getUrlsBin = async () => {
 	try {
 		const { data } = await axios({
 			method: "GET",
-			url: "window.location.origin" + MY_BIN_ID_FOR_URLS,
+			url: window.location.origin + MY_BIN_ID_FOR_URLS,
 			data: {},
 		});
 		// There are 2 layers of arrays in Koren's bins for some reason
--- public/utils.js
+++ public/utils.js
@@ -1,4 +1,4 @@
-const URL = "window.location.origin";
+const URL = window.location.origin;
 
 async function makeUrlShort(longUrl) {
 	try {
